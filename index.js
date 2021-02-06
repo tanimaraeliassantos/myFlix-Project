77cc47ca1379e3c3169866ecaa56638e21a3da77
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-02-06 20:16:48 +1100

    Update index.js
    Create express code to route all endpoints determined at documentation

--- index.js
+++ index.js
@@ -114,12 +114,306 @@
 // 	console.log('Your app is listening on port 8080.');
 // });
 
+// EXERCISE 2.4
+
+// const express = require('express'),
+// 	morgan = require('morgan');
+
+// const app = express();
+
+// app.use(morgan('common'));
+// app.use(express.static('public'));
+
+// app.use((err, req, res, next) => {
+// 	console.error(err.stack);
+// 	res.status(500).send('Something broke!');
+// });
+
+// let topMovies = [
+// 	{
+// 		title: 'Black Panther',
+// 		year: '2018',
+// 		genre: [
+// 			{
+// 				slot: 1,
+// 				genre: 'fantasy',
+// 			},
+// 			{
+// 				slot: 2,
+// 				genre: 'action',
+// 			},
+// 			{
+// 				slot: 3,
+// 				genre: 'adventure',
+// 			},
+// 		],
+
+// 		director: 'Ryan Coogler',
+// 	},
+// 	{
+// 		title: 'Spider-man: into the spider-verse',
+// 		year: '2018',
+// 		genre: [
+// 			{
+// 				slot: 1,
+// 				genre: 'animation',
+// 			},
+// 			{
+// 				slot: 2,
+// 				genre: 'fantasy',
+// 			},
+// 			{
+// 				slot: 3,
+// 				genre: 'comedy',
+// 			},
+// 			{
+// 				slot: 4,
+// 				genre: 'adventure',
+// 			},
+// 			{
+// 				slot: 5,
+// 				genre: 'action',
+// 			},
+// 			{
+// 				slot: 6,
+// 				genre: 'kids and family',
+// 			},
+// 		],
+
+// 		director: [
+// 			{
+// 				slot: 1,
+// 				director: 'Bob Persichetti',
+// 			},
+// 			{
+// 				slot: 2,
+// 				director: 'Peter Ramsey',
+// 			},
+// 			{
+// 				slot: 3,
+// 				director: 'Rodney Rothman',
+// 			},
+// 		],
+// 	},
+// 	{
+// 		title: 'The Incredibles',
+// 		year: '2004',
+// 		genre: [
+// 			{
+// 				slot: 1,
+// 				genre: 'animation',
+// 			},
+// 			{
+// 				slot: 2,
+// 				genre: 'comedy',
+// 			},
+// 			{
+// 				slot: 3,
+// 				genre: 'kids and family',
+// 			},
+// 			{
+// 				slot: 4,
+// 				genre: 'adventure',
+// 			},
+// 		],
+
+// 		director: 'Brad Bird',
+// 	},
+// 	{
+// 		title: 'Avengers: Endgame',
+// 		year: '2019',
+// 		genre: [
+// 			{
+// 				slot: 1,
+// 				genre: 'sci fi',
+// 			},
+// 			{
+// 				slot: 2,
+// 				genre: 'fantasy',
+// 			},
+// 			{
+// 				slot: 3,
+// 				genre: 'action',
+// 			},
+// 			{
+// 				slot: 4,
+// 				genre: 'adventure',
+// 			},
+// 		],
+
+// 		director: [
+// 			{
+// 				slot: 1,
+// 				director: 'Anthony Russo',
+// 			},
+// 			{
+// 				slot: 2,
+// 				director: 'Joe Russo',
+// 			},
+// 		],
+// 	},
+// 	{
+// 		title: 'The Dark Knight',
+// 		year: '2008',
+// 		genre: [
+// 			{
+// 				slot: 1,
+// 				genre: 'fantasy',
+// 			},
+// 			{
+// 				slot: 2,
+// 				genre: 'action',
+// 			},
+// 			{
+// 				slot: 3,
+// 				genre: 'adventure',
+// 			},
+// 		],
+
+// 		director: 'Cristopher Nolan',
+// 	},
+// 	{
+// 		title: 'Iron Man',
+// 		year: '2008',
+// 		genre: [
+// 			{
+// 				slot: 1,
+// 				genre: 'sci fi',
+// 			},
+// 			{
+// 				slot: 2,
+// 				genre: 'fantasy',
+// 			},
+// 			{
+// 				slot: 3,
+// 				genre: 'action',
+// 			},
+// 			{
+// 				slot: 4,
+// 				genre: 'adventure',
+// 			},
+// 		],
+
+// 		director: 'Jon Favreau',
+// 	},
+// 	{
+// 		title: 'Superman: The Movie',
+// 		year: '1978',
+// 		genre: [
+// 			{
+// 				slot: 1,
+// 				genre: 'sci fi',
+// 			},
+// 			{
+// 				slot: 2,
+// 				genre: 'fantasy',
+// 			},
+// 			{
+// 				slot: 3,
+// 				genre: 'action',
+// 			},
+// 			{
+// 				slot: 4,
+// 				genre: 'adventure',
+// 			},
+// 		],
+
+// 		director: 'Richard Donner',
+// 	},
+// 	{
+// 		title: 'Wonder Woman',
+// 		year: '2017',
+// 		genre: [
+// 			{
+// 				slot: 1,
+// 				genre: 'fantasy',
+// 			},
+// 			{
+// 				slot: 2,
+// 				genre: 'action',
+// 			},
+// 			{
+// 				slot: 3,
+// 				genre: 'adventure',
+// 			},
+// 		],
+
+// 		director: 'Patty Jenkins',
+// 	},
+// 	{
+// 		title: 'Thor: Ragnarok',
+// 		year: '2017',
+// 		genre: [
+// 			{
+// 				slot: 1,
+// 				genre: 'fantasy',
+// 			},
+// 			{
+// 				slot: 2,
+// 				genre: 'sci fi',
+// 			},
+// 			{
+// 				slot: 3,
+// 				genre: 'comedy',
+// 			},
+// 			{
+// 				slot: 4,
+// 				genre: 'adventure',
+// 			},
+// 			{
+// 				slot: 5,
+// 				genre: 'action',
+// 			},
+// 		],
+
+// 		director: 'Taika Waititi',
+// 	},
+// 	{
+// 		title: 'Logan',
+// 		year: '2017',
+// 		genre: [
+// 			{
+// 				slot: 1,
+// 				genre: 'fantasy',
+// 			},
+// 			{
+// 				slot: 2,
+// 				genre: 'action',
+// 			},
+// 			{
+// 				slot: 3,
+// 				genre: 'adventure',
+// 			},
+// 		],
+
+// 		director: 'James Mangold',
+// 	},
+// ];
+
+// //GET requests
+// app.get('/', (req, res) => {
+// 	res.send('Welcome to myFlix!');
+// });
+
+// app.get('/movies', (req, res) => {
+// 	res.json(topMovies);
+// });
+
+// app.listen(8080, () => {
+// 	console.log('Your app is listening on port 8080.');
+// });
+
+// EXERCISE 2.5
+
 const express = require('express'),
+	bodyParser = require('body-parser'),
+	uuid = require('uuid'),
 	morgan = require('morgan');
 
 const app = express();
 
 app.use(morgan('common'));
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.use((err, req, res, next) => {
@@ -146,7 +440,7 @@ let topMovies = [
 			},
 		],
 
-		director: 'Ryan Coogler',
+		directors: 'Ryan Coogler',
 	},
 	{
 		title: 'Spider-man: into the spider-verse',
@@ -178,7 +472,7 @@ let topMovies = [
 			},
 		],
 
-		director: [
+		directors: [
 			{
 				slot: 1,
 				director: 'Bob Persichetti',
@@ -215,7 +509,7 @@ let topMovies = [
 			},
 		],
 
-		director: 'Brad Bird',
+		directors: 'Brad Bird',
 	},
 	{
 		title: 'Avengers: Endgame',
@@ -239,7 +533,7 @@ let topMovies = [
 			},
 		],
 
-		director: [
+		directors: [
 			{
 				slot: 1,
 				director: 'Anthony Russo',
@@ -268,7 +562,7 @@ let topMovies = [
 			},
 		],
 
-		director: 'Cristopher Nolan',
+		directors: 'Cristopher Nolan',
 	},
 	{
 		title: 'Iron Man',
@@ -292,7 +586,7 @@ let topMovies = [
 			},
 		],
 
-		director: 'Jon Favreau',
+		directors: 'Jon Favreau',
 	},
 	{
 		title: 'Superman: The Movie',
@@ -316,7 +610,7 @@ let topMovies = [
 			},
 		],
 
-		director: 'Richard Donner',
+		directors: 'Richard Donner',
 	},
 	{
 		title: 'Wonder Woman',
@@ -336,7 +630,7 @@ let topMovies = [
 			},
 		],
 
-		director: 'Patty Jenkins',
+		directors: 'Patty Jenkins',
 	},
 	{
 		title: 'Thor: Ragnarok',
@@ -364,7 +658,7 @@ let topMovies = [
 			},
 		],
 
-		director: 'Taika Waititi',
+		directors: 'Taika Waititi',
 	},
 	{
 		title: 'Logan',
@@ -384,17 +678,106 @@ let topMovies = [
 			},
 		],
 
-		director: 'James Mangold',
+		directors: 'James Mangold',
 	},
 ];
 
-//GET requests
+//Welcoming message
 app.get('/', (req, res) => {
 	res.send('Welcome to myFlix!');
 });
 
+//GET the list of ALL movies
+
 app.get('/movies', (req, res) => {
 	res.json(topMovies);
+});
+
+//GET the data about a single movie, by title
+
+app.get('/movies/:title', (req, res) => {
+	res.json(
+		topMovies.find((movie) => {
+			return movie.title === req.params.name;
+		})
+	);
+});
+
+//Add a movie to list of movies
+
+app.post('/movies', (req, res) => {
+	let newMovie = req.body;
+
+	if (!newMovie.title) {
+		const message = 'Missing title in request body';
+		res.status(400).send(message);
+	} else {
+		newMovie.id = uuid.v4();
+		topMovies.push(newMovie);
+		res.status(201).send(newMovie);
+	}
+});
+
+// Delete a movie by ID
+
+app.delete('/movies/:id', (req, res) => {
+	let movie = topMovies.find((movie) => {
+		return movie.id === req.params.id;
+	});
+
+	if (movie) {
+		topMovies = topMovies.filter((obj) => {
+			return obj.id !== req.params.id;
+		});
+		res.status(201).send('Movie ' + req.params.id + ' was deleted.');
+	}
+});
+
+// Update a movie title in a certain year
+
+app.put('/movies/:title/:year', (req, res) => {
+	let movie = topMovies.find((movie) => {
+		return movie.title === req.params.title;
+	});
+
+	if (movie) {
+		movie.titles[req.params.title] = parseInt(req.params.year);
+		res
+			.status(201)
+			.send(
+				'Movie ' +
+					req.params.title +
+					' was released in ' +
+					req.params.year +
+					' . '
+			);
+	} else {
+		res
+			.status(404)
+			.send('Movie with the title ' + req.params.title + ' was not found.');
+	}
+});
+
+// GET the directors of a movie
+app.get('movies/:title/directors', (req, res) => {
+	let movie = topMovies.find((movie) => {
+		return movie.title === req.params.title;
+	});
+
+	if (movie) {
+		let movieYear = topMovies.find((movie) => {
+			return movie.year === req.params.year;
+		});
+
+		console.log(movie);
+		console.log(movie.year);
+		res.status(201).send('' + year);
+		// res.status(201).send(year);
+	} else {
+		res
+			.status(404)
+			.send('Movie witht the title ' + req.params.title + ' was not found.');
+	}
 });
 
 app.listen(8080, () => {
