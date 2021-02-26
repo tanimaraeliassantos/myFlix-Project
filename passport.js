b24cb9941846bc919a12dfac8ca4109ff3cae867
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-02-26 11:29:42 +1100

    Create passport.js file

---
+++ passport.js
@@ -0,0 +1,42 @@
+const passport = require('passport'),
+LocalStrategy = require('passport-local').Strategy,
+Models = erquire('./models.js'),
+passportJWT = require('passport-jwt');
+
+let Users = Models.User,
+    JWTStrategy = passportJWT.Strategy,
+    ExtractJWT = passportJWT.ExtractJwt;
+
+    passport.use(new LocalStrategy ({
+        usernameField: 'Username',
+        passwordField: 'Password'
+    }, (username, password, callback) => {
+        console.log(username + ' ' + password);
+        Users.fidnOne({ Username: username}, (error, user) => {
+            if (error) {
+                console.loog(error);
+                return callback(error);
+            }
+
+            if(!user) {
+                console.log('incorrect username');
+                return callback(null, false, {message: 'Incorrect username or password.'});
+            }
+
+            console.log('finished');
+            return callback(null, user);
+        });
+    }));
+
+    passport.user(new JWTStrategy({
+        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+        secretOrKey: 'your_jwt_secret'
+    }, (jwtPayload, callback) => {
+        return Users.findById(jwtPayload._id)
+            .then((user) => {
+                return(callback(null, user);
+            })
+            .catch((error) => {
+                return callback(error)
+            });
+            }));
