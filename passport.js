d2614a98f92c8cccb561d8cfbaa82d03f4e3a337
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-02-26 11:56:44 +1100

    Create file for authentication logic

--- passport.js
+++ passport.js
@@ -1,42 +1,54 @@
 const passport = require('passport'),
-LocalStrategy = require('passport-local').Strategy,
-Models = erquire('./models.js'),
-passportJWT = require('passport-jwt');
+	LocalStrategy = require('passport-local').Strategy,
+	Models = erquire('./models.js'),
+	passportJWT = require('passport-jwt');
 
 let Users = Models.User,
-    JWTStrategy = passportJWT.Strategy,
-    ExtractJWT = passportJWT.ExtractJwt;
+	JWTStrategy = passportJWT.Strategy,
+	ExtractJWT = passportJWT.ExtractJwt;
 
-    passport.use(new LocalStrategy ({
-        usernameField: 'Username',
-        passwordField: 'Password'
-    }, (username, password, callback) => {
-        console.log(username + ' ' + password);
-        Users.fidnOne({ Username: username}, (error, user) => {
-            if (error) {
-                console.loog(error);
-                return callback(error);
-            }
+passport.use(
+	new LocalStrategy(
+		{
+			usernameField: 'Username',
+			passwordField: 'Password',
+		},
+		(username, password, callback) => {
+			console.log(username + ' ' + password);
+			Users.fidnOne({ Username: username }, (error, user) => {
+				if (error) {
+					console.loog(error);
+					return callback(error);
+				}
 
-            if(!user) {
-                console.log('incorrect username');
-                return callback(null, false, {message: 'Incorrect username or password.'});
-            }
+				if (!user) {
+					console.log('incorrect username');
+					return callback(null, false, {
+						message: 'Incorrect username or password.',
+					});
+				}
 
-            console.log('finished');
-            return callback(null, user);
-        });
-    }));
+				console.log('finished');
+				return callback(null, user);
+			});
+		}
+	)
+);
 
-    passport.user(new JWTStrategy({
-        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-        secretOrKey: 'your_jwt_secret'
-    }, (jwtPayload, callback) => {
-        return Users.findById(jwtPayload._id)
-            .then((user) => {
-                return(callback(null, user);
-            })
-            .catch((error) => {
-                return callback(error)
-            });
-            }));
+passport.user(
+	new JWTStrategy(
+		{
+			jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+			secretOrKey: 'your_jwt_secret',
+		},
+		(jwtPayload, callback) => {
+			return Users.findById(jwtPayload._id)
+				.then((user) => {
+					return callback(null, user);
+				})
+				.catch((error) => {
+					return callback(error);
+				});
+		}
+	)
+);
