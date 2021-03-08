9be4c5d2af4c9cee40b36e9627b52dc31c2bc3b3
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-03-08 16:00:39 +1100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,10 +1,11 @@
 const jwtSecret = 'your_jwt_secret';
+
 const jwt = require('jsonwebtoken'),
-	passport = require('passport');
+const passport = require('passport');
 
-require('./passport');
+require('./passport.js');
 
-let generateJWTToken = (user) => {
+const generateJWTToken = (user) => {
 	return jwt.sign(user, jwtSecret, {
 		subject: user.username,
 		expiresIn: '7d',
@@ -15,19 +16,19 @@ let generateJWTToken = (user) => {
 /* POST login. */
 module.exports = (router) => {
 	router.post('/login', (req, res) => {
-		passport.authenticate('local', { session: false }, (error, user, info) => {
+		passport.authenticate('local', {session: false}, (error, user, info) => {
 			if (error || !user) {
 				return res.status(400).json({
 					message: 'Something is not right',
 					user: user,
 				});
 			}
-			req.login(user, { session: false }, (error) => {
+			req.login(user, {session: false}, (error) => {
 				if (error) {
 					res.send(error);
 				}
-				let token = generateJWTToken(user.toJSON());
-				return res.json({ user, token });
+				const token = generateJWTToken(user.toJSON());
+				return res.json({user, token});
 			});
 		})(req, res);
 	});
