185a6f64b72a3bb94188e9b7aba35d5d82140493
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-03-10 14:28:23 +1100

    Update files

--- index.js
+++ index.js
@@ -216,7 +216,7 @@ app.post(
 				if (User) {
 					return res.status(400).send(req.body.Username + ' already exists');
 				} else {
-					User.create({
+					Users.create({
 						Username: req.body.Username,
 						Password: hashedPassword,
 						Email: req.body.Email,
