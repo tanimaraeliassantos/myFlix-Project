2b048973818da8b5e1d5d54f057cea1d0137ae80
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-03-10 13:38:19 +1100

    update auth.js

--- auth.js
+++ auth.js
@@ -1,39 +1,33 @@
 const jwtSecret = 'your_jwt_secret';
-// This has to be the same key used in JWTStrategy
-
 const jwt = require('jsonwebtoken'),
 	passport = require('passport');
 
 require('./passport');
-// Local passport file
 
-let generateJWTToken = (user) => {
-	return jwt.sign(user, jwtSecret, {
-		subject: user.Username,
-		// This is the username you are encoding in the JWT
+let generateJWTToken = (Users) => {
+	return jwt.sign(Users, jwtSecret, {
+		subject: Users.Username,
 		expiresIn: '7d',
-		// This specifies that the token will expire in 7 days
 		algorithm: 'HS256',
-		// this is the algorithm used to "sign" or encode the values of the JWT
 	});
 };
 
 /* POST login. */
 module.exports = (router) => {
 	router.post('/login', (req, res) => {
-		passport.authenticate('local', { session: false }, (error, user, info) => {
-			if (error || !user) {
+		passport.authenticate('local', { session: false }, (error, Users, info) => {
+			if (error || !Users) {
 				return res.status(400).json({
 					message: 'Something is not right',
-					user: user,
+					Users: Users,
 				});
 			}
-			req.login(user, { session: false }, (error) => {
+			req.login(Users, { session: false }, (error) => {
 				if (error) {
 					res.send(error);
 				}
-				let token = generateJWTToken(user.toJSON());
-				return res.json({ user, token });
+				let token = generateJWTToken(Users.toJSON());
+				return res.json({ Users, token });
 			});
 		})(req, res);
 	});
