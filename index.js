9a2d6853514227d8e364835a7926bd43bd5428bc
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-03-24 11:51:15 +1100

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ let allowedOrigins = [
 	'http://localhost:8080',
 	'http://testsite.com',
 	'http://localhost:1234',
+	'*',
 ];
 
 const { check, validationResult } = require('express-validator');
